cbafde2b43efa5015fe39b99f590960c31c56aa6
Ethan Rosenberg <ethan@ethanrosenberg.com>
2019-02-07 22:51:39 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,8 +3,18 @@ require 'pry'
 
 class Scraper
 
-  def self.scrape_index_page(index_url)
-    
+   def self.scrape_index_page(index_url)
+   index_data = []
+    doc = Nokogiri::HTML(open(index_url))
+     students = doc.css("div .student-card")
+
+     students.each do |student|
+     
+       name = student.css("a .card-text-container .student-name").text
+       location = student.css("a .card-text-container .student-location").text
+       index_data << {:name => name, :location => location}
+     end
+     index_data
   end
 
   def self.scrape_profile_page(profile_url)
