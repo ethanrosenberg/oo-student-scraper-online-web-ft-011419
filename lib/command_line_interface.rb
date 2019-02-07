d80ddd07c02984aa3c8837f806ba6664ca09c282
Ethan Rosenberg <ethan@ethanrosenberg.com>
2019-02-07 23:46:09 +0000

    Automatically backed up by Learn

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -2,7 +2,7 @@ require_relative "../lib/scraper.rb"
 require_relative "../lib/student.rb"
 require 'nokogiri'
 require 'colorize'
-require'pry
+require'pry'
 
 class CommandLineInterface
   BASE_PATH = "./fixtures/student-site/"
