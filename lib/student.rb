69287543d5a9bcdaa99de0076da61cc3f0e5cebe
Ethan Rosenberg <ethan@ethanrosenberg.com>
2019-02-07 23:33:16 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -18,6 +18,11 @@ class Student
 
   def add_student_attributes(attributes_hash)
     @twitter = attributes_hash[:twitter]
+    @linkedin = attributes_hash[:linkedin]
+    @github = attributes_hash[:github]
+    @blog = attributes_hash[:blog]
+    @profile_quote attributes_hash[:profile_quote]
+    @bio = attributes_hash[:bio]
   end
 
   def self.all
